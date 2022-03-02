b40c04aec0e3ace75deca14a67f35ee388a54c3b
parikshya098 <pbhandari1@ualr.edu>
2022-03-02 15:57:25 +0000

    Unit 7

---
+++ 07.01 Slice a String.py
@@ -0,0 +1,11 @@
+var1 = str(input("Enter a string: "))
+
+print(var1[2])
+print(var1[-2]) 
+print(var1[0:5])
+print(var1[0:-2])
+print(var1[0::2])
+print(var1[1::2])
+print(var1[::-1])
+print(var1[::-2])
+print(len(var1))
