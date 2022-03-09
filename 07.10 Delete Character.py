6da424e80e5b15b90e89c085ffb55a555681339d
parikshya098 <pbhandari1@ualr.edu>
2022-03-09 07:27:32 +0000

    Unit 7

--- 07.10 Delete Character.py
+++ 07.10 Delete Character.py
@@ -1,6 +1,4 @@
-user_string = input("Enter a string: ") # Prompt the user to enter a string.
-
-# Loop that checks each letter in the user's string.
+user_string = input("Enter a string: ") 
 for i in range(len(user_string)):
-    if(user_string[i] != "@"): # IF condition that only prints out letters that are '@'
-        print(user_string[i], end = "") # Prints the letters in one line, with no space.
+    if(user_string[i] != "@"): 
+        print(user_string[i], end = "") 
