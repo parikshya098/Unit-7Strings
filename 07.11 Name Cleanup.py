200a53cccd36e987335d76da3ca70f7294b2f98a
parikshya098 <pbhandari1@ualr.edu>
2022-03-09 15:35:15 +0000

    Unit 7

--- 07.11 Name Cleanup.py
+++ 07.11 Name Cleanup.py
@@ -1,5 +1,3 @@
-#Returns a properly cases string s by uppercasing the first character
-# and lowercasing the rest of the string.
 def ProperCase(s):
     str = ''
     str += s[0].upper()
@@ -7,26 +5,16 @@ def ProperCase(s):
         str += s[i].lower()
     s = str
     return s
-
-#Returns a string with the NewLine Character ("\n") removed from string s
 def RemoveNewLine(s):
     return s.strip('\n')
-
-#Returns a string with the leading and trailing spaces removed from string s
 def Trim(s):
     return s.strip()
-
-#Returns the first name of string s
 def FirstName(s):
     i = s.find(' ')
     return ProperCase(s[0: i])
-
-#Returns the last name of string s
 def LastName(s):
     i = s.rfind(' ')
     return ProperCase(s[i+1 :])
-
-#Returns the middle name of string s
 def MiddleName(s):
     first = s.find(' ')
     last = s.rfind(' ')
@@ -37,14 +25,10 @@ def MiddleName(s):
     else:
         return ProperCase(Trim(s[first+1:last]))
 
-
-#Open the file
 f = open("07.11 Names.txt", "r")
-
 print("%-10s %-10s %-10s" %("First", "Middle", "Last"))
 print("%-10s %-10s %-10s" %("----------", "----------", "----------"))
 
-#Read file line by line
 for x in f:
     s = Trim(x)
     s = RemoveNewLine(s)
